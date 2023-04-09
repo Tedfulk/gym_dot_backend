d6c43a8ee44d0cac017c1dcb25b72d5a3acdfc61
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2023-04-09 03:32:07 -0400

    WIP: lessons_api added to fastapi

--- api/lesons_api.py
+++ api/lesons_api.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import date, time
 from uuid import UUID
 
 from fastapi import APIRouter
@@ -25,7 +26,61 @@ def get_all_lessons():
     return asyncio.run(lessons)
 
 
-# @app.get("/{lesson_id}", response_model=GetLessonResult)
-# def get_lesson_by_id(lesson_id: UUID):
-#     lesson = get_lesson(executor=client, lesson_id=lesson_id)
-#     return asyncio.run(lesson)
+@app.get("/{lesson_id}", response_model=GetLessonResult)
+def get_lesson_by_id(lesson_id: UUID):
+    lesson = get_lesson(executor=client, lesson_id=lesson_id)
+    return asyncio.run(lesson)
+
+
+@app.post("", response_model=CreateLessonResult)
+def make_lesson(
+    class_dates: list[date],
+    class_times: list[time],
+    len_of_class_time: int,
+    active: bool,
+    max_attendees: int,
+    min_attendees: int,
+    waitlist: int,
+):
+    lesson = create_lesson(
+        executor=client,
+        class_dates=class_dates,
+        class_times=class_times,
+        len_of_class_time=len_of_class_time,
+        active=active,
+        max_attendees=max_attendees,
+        min_attendees=min_attendees,
+        waitlist=waitlist,
+    )
+    return asyncio.run(lesson)
+
+
+@app.put("/{lesson_id}", response_model=UpdateLessonResult)
+def update_lesson_by_id(
+    lesson_id: UUID,
+    class_dates: list[date],
+    class_times: list[time],
+    len_of_class_time: int,
+    active: bool,
+    max_attendees: int,
+    min_attendees: int,
+    waitlist: int,
+):
+    lesson = update_lesson(
+        executor=client,
+        lesson_id=lesson_id,
+        class_dates=class_dates,
+        class_times=class_times,
+        len_of_class_time=len_of_class_time,
+        active=active,
+        max_attendees=max_attendees,
+        min_attendees=min_attendees,
+        waitlist=waitlist,
+    )
+    return asyncio.run(lesson)
+
+
+@app.delete("/{lesson_id}", response_model=DeleteLessonResult)
+def delete_lesson_by_id(lesson_id: UUID):
+    lesson = delete_lesson(executor=client, lesson_id=lesson_id)
+    return asyncio.run(lesson)
