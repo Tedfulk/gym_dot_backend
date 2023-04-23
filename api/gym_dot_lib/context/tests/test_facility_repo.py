c45b60b16a90b68cc4cc6e6ec84e9ce473b7c4ab
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2023-04-23 09:02:50 -0400

    test: facility repo test passing

--- api/gym_dot_lib/context/tests/test_facility_repo.py
+++ api/gym_dot_lib/context/tests/test_facility_repo.py
@@ -1,12 +1,11 @@
 import pytest
 
 from api.gym_dot_lib.context.facilities import (
-    CreateFacilityResult,
-    GetFacilityResult,
-    UpdateFacilityResult,
-    delete_facility,
-    get_facility,
-    update_facility,
+    DeleteFacilityResult,
+    Facility,
+    FacilityRepo,
+    FacilityUpdates,
+    NewFacility,
 )
 from api.gym_dot_lib.context.main import client
 from api.sample_data import *
@@ -15,39 +14,50 @@ from api.sample_data import *
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_facility(sample_facility: CreateFacilityResult):
-    facility = await get_facility(
+async def test_get_facility(sample_facility: Facility):
+    facility = await FacilityRepo.get(
         executor=client,
         facility_id=sample_facility.id,
     )
     assert facility is not None
-    assert facility == CreateFacilityResult(**sample_facility.dict())
+    assert facility == Facility(**sample_facility.dict())
 
 
-async def test_update_facility(sample_facility: CreateFacilityResult):
+async def test_update_facility(sample_facility: Facility):
     new_name = sample_facility.name + " (updated)"
-    updates = UpdateFacilityResult(**{**sample_facility.dict(), "name": new_name})
-    updated_facility = await update_facility(
+    updates = FacilityUpdates(**{**sample_facility.dict(), "name": new_name})
+    updated_facility = await FacilityRepo.update(
         executor=client,
         facility_id=sample_facility.id,
-        name=updates.name,
-        address=updates.address,
-        city=updates.city,
-        state=updates.state,
+        updates=FacilityUpdates(
+            name=updates.name,
+            address=updates.address,
+            city=updates.city,
+            state=updates.state,
+        ),
     )
     assert updated_facility is not None
-    assert CreateFacilityResult(**sample_facility.dict()) != UpdateFacilityResult(
+    assert Facility(**sample_facility.dict()) != FacilityUpdates(
         **updated_facility.dict()
     )
 
 
-async def test_delete_facility(sample_facility: CreateFacilityResult):
-    deleted_facility = await delete_facility(
+async def test_delete_facility():
+    sample_facility = await FacilityRepo.create(
+        executor=client,
+        new_facility=NewFacility(
+            name="Sample Facility",
+            address="123 Main St.",
+            city="Anytown",
+            state="CA",
+        ),
+    )
+    deleted_facility = await FacilityRepo.delete(
         executor=client,
         facility_id=sample_facility.id,
     )
     assert deleted_facility is not None
-    facility = await get_facility(
+    facility = await FacilityRepo.get(
         executor=client,
         facility_id=deleted_facility.id,
     )
