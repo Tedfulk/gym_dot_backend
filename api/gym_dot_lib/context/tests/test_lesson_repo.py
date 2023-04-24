ecec09c1afa8e33721de151d58e50c0a0508df0a
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2023-04-24 13:44:49 -0400

    test: lesson repo test passing

--- api/gym_dot_lib/context/tests/test_lesson_repo.py
+++ api/gym_dot_lib/context/tests/test_lesson_repo.py
@@ -14,51 +14,71 @@ from api.sample_data import *
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_lesson(sample_lesson: CreateLessonResult):
-    lesson = await get_lesson(
+async def test_get_lesson(sample_lesson: Lesson):
+    lesson = await LessonRepo.get(
         executor=client,
         lesson_id=sample_lesson.id,
     )
     if lesson is not None:
-        assert GetLessonResult(**lesson.dict()) == CreateLessonResult(
-            **sample_lesson.dict()
-        )
+        assert Lesson(**lesson.dict()) == Lesson(**sample_lesson.dict())
 
 
-async def test_update_lesson(sample_lesson: CreateLessonResult):
-    updated_lesson = await update_lesson(
+async def test_update_lesson(sample_lesson: Lesson):
+    updated_lesson = await LessonRepo.update(
         executor=client,
         lesson_id=sample_lesson.id,
-        class_dates=[
-            date.today(),
-            date.today() + timedelta(days=10),
-            date.today() + timedelta(days=20),
-        ],
-        class_times=[
-            time(1, 0),
-            time(2, 30),
-            time(3, 0),
-            time(4, 30),
-            time(5, 30),
-            time(6, 30),
-        ],
-        len_of_class_time=600,
-        active=False,
-        max_attendees=200,
-        min_attendees=100,
-        waitlist=1,
+        updates=LessonUpdates(
+            class_dates=[
+                date.today(),
+                date.today() + timedelta(days=10),
+                date.today() + timedelta(days=20),
+            ],
+            class_times=[
+                time(1, 0),
+                time(2, 30),
+                time(3, 0),
+                time(4, 30),
+                time(5, 30),
+                time(6, 30),
+            ],
+            len_of_class_time=600,
+            active=False,
+            max_attendees=200,
+            min_attendees=100,
+            waitlist=1,
+        ),
     )
     if updated_lesson is not None:
-        assert GetLessonResult(**sample_lesson.dict()) != UpdateLessonResult(
-            **updated_lesson.dict()
-        )
+        assert Lesson(**sample_lesson.dict()) != Lesson(**updated_lesson.dict())
 
 
-async def test_delete_lesson(sample_lesson: CreateLessonResult):
-    deleted_lesson = await delete_lesson(
+async def test_delete_lesson():
+    sample_lesson = await LessonRepo.create(
+        executor=client,
+        new_lesson=NewLesson(
+            class_dates=[
+                date.today(),
+                date.today() + timedelta(days=1),
+                date.today() + timedelta(days=2),
+            ],
+            class_times=[
+                time(6, 0),
+                time(9, 30),
+                time(12, 0),
+                time(16, 30),
+                time(17, 30),
+                time(18, 30),
+            ],
+            len_of_class_time=60,
+            max_attendees=20,
+            min_attendees=1,
+            waitlist=10,
+        ),
+    )
+    deleted_lesson = await LessonRepo.delete(
         executor=client,
         lesson_id=sample_lesson.id,
     )
-    assert deleted_lesson is not None
-    lesson = await get_lesson(executor=client, lesson_id=sample_lesson.id)
+    assert DeleteLessonResult(**deleted_lesson.dict()) is not None
+    lesson = await LessonRepo.get(executor=client, lesson_id=sample_lesson.id)
     assert lesson is None
